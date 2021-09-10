5a759bbeb51931e38ce7f2340fc9a67530aadc0f
Rishi Verma <rishi.verma21803@gmail.com>
2021-09-10 23:20:18 +0530

    now can add and view data from database

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, session, flash, redirect, url_for, render_template, request
-from flask.scaffold import F
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MYSECRETKEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///info.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+db.drop_all()
+db.create_all()
 
 
 @app.route('/')
@@ -12,13 +28,24 @@ def home():
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    data = request.args.get('data')
-    # return render_template('register.html')
-    return f'the data is {data}'
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        row = User(first_name=fname, last_name=lname, email=email)
+        db.session.add(row)
+        db.session.commit()
+        flash('successful')
+        return redirect(url_for('register'))
+    else:
+        return render_template('register.html')
 
 
-@app.route('/view')
+@app.route('/view', methods=['POST', 'GET'])
 def view():
+    if request.method == 'POST':
+        row = User.query.filter_by(email=request.form['email']).first()
+        return render_template('view.html', user=row)
     return render_template('view.html')
 
 
